60818bf6efb5ddbff00be85e37d8eb767ab5e7c0
Carlos Vozmediano <carlosvozmedianoruiz@gmail.com>
2025-02-16 20:32:17 +0100

    Fullscreen activity upgrade

--- app/src/main/java/com/vozmediano/vozmedianonasa/ui/FullscreenActivity.kt
+++ app/src/main/java/com/vozmediano/vozmedianonasa/ui/FullscreenActivity.kt
@@ -10,6 +10,7 @@ import com.vozmediano.vozmedianonasa.databinding.ActivityFullscreenBinding
 class FullscreenActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityFullscreenBinding.inflate(layoutInflater) }
+    var fullscreen = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,5 +31,28 @@ class FullscreenActivity : AppCompatActivity() {
             .load(hdurl)
             .error(R.drawable.baseline_image_not_supported_24)
             .into(binding.fullscreenImageView)
+
+        binding.fullscreenImageView.setOnClickListener {
+            fullscreenSwap(fullscreen)
+            fullscreen = !fullscreen
+        }
+    }
+
+    fun fullscreenSwap(fullscreen: Boolean) {
+        if (fullscreen) {
+            window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+            )
+        } else {
+            window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            )
+        }
+
     }
 }
